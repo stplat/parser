aecd6a73d8f7153465ee801c48d4bd39ed14a405
platonovso <st.platonov@gmail.com>
2020-01-22 10:42:20 +0300

    категории (офис)

--- update_db/add_categories.php
+++ update_db/add_categories.php
@@ -10,13 +10,35 @@ $connect = new mysqli("localhost", "root", "", "vecheria_laravel");
 //Кодировка данных получаемых из базы
 $connect->query("SET NAMES 'utf8' ");
 
+function translit($str) {
+  $rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+  $lat = array('A', 'B', 'V', 'G', 'D', 'E', 'Yo', 'Zh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'Kh', 'Ts', 'Ch', 'Sh', 'Sch', '', 'Y', '', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'yo', 'zh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'kh', 'c', 'ch', 'sh', 'sch', '', 'y', '', 'e', 'yu', 'ya');
+  return str_replace($rus, $lat, $str);
+}
 
 $excel = PHPExcel_IOFactory::load('category.xlsx');
+$excel->setActiveSheetIndex(0);
+$sheet = $excel->getActiveSheet();
+$objWriter = new PHPExcel_Writer_Excel2007($excel);
 
 $maxCell = $excel->getActiveSheet()->getHighestRowAndColumn();
 $data = $excel->getActiveSheet()->rangeToArray('A1:' . $maxCell['column'] . $maxCell['row']);
 
-$query = "INSERT INTO `categories` (`id`, `category`, `subcategory`, `plug`, `available`, `meta_keywords`, `meta_description`, `meta_title`, `created_at`, `updated_at`) VALUES ";
+foreach ($data as $key => $row) {
+  foreach ($row as $i => $col) {
+    if ($key != 0 && $i == 2) {
+      $category = mb_strtolower(translit($col));
+      $category = str_replace(' ', '-', $category);
+      $sheet->setCellValue('D' . (1 + $key), trim($category));
+    }
+  }
+}
+
+$objWriter->save(__DIR__ . '/category.xlsx');
+$excel->disconnectWorksheets();
+unset($objWriter, $excel);
+
+/*$query = "INSERT INTO `categories` (`id`, `category`, `subcategory`, `plug`, `available`, `meta_keywords`, `meta_description`, `meta_title`, `created_at`, `updated_at`) VALUES ";
 
 
 foreach ($data as $key => $row) {
@@ -32,9 +54,9 @@ foreach ($data as $key => $row) {
 }
 
 $query = preg_replace('/, $/', '', $query);
-echo $query;
+echo $query;*/
 
-$result = $connect->query($query);
+//$result = $connect->query($query);
 
 
 
