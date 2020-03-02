8e5f747097c69ae369a67eeaa708e5c1d62c0006
platonovso <st.platonov@gmail.com>
2020-03-02 17:01:18 +0300

    Парсер (офис)

--- update_db/add_product.php
+++ update_db/add_product.php
@@ -16,7 +16,7 @@ $excel = PHPExcel_IOFactory::load('product.xlsx');
 $maxCell = $excel->getActiveSheet()->getHighestRowAndColumn();
 $data = $excel->getActiveSheet()->rangeToArray('A1:' . $maxCell['column'] . $maxCell['row']);
 
-$query = "INSERT INTO `product` (`product_id`, `category_id`, `name`, `slug`, `manufacturer`, `article`, `meta_keywords`, `meta_description`, `meta_title`, `available`, `weight`, `price`, `dimension`, `comment`, `material`, `technic`, `description`, `image_path`, `created_at`, `updated_at`) VALUES ";
+$query = "INSERT INTO `product` (`product_id`, `category_id`, `name`, `slug`, `manufacturer`, `article`, `meta_keywords`, `meta_description`, `meta_title`, `available`, `weight`, `price`, `dimension`, `comment`, `material`, `technic`, `description`, `video`, `image_path`, `created_at`, `updated_at`) VALUES ";
 
 
 foreach ($data as $key => $row) {
